5e6d750d66babd4eb38e783320b6bfd7e6dddb1e
andriw <andfante@misena.edu/co>
2022-04-22 00:01:46 -0500

    reducion de codigo

--- Agsoftware/Header/registration.php
+++ Agsoftware/Header/registration.php
@@ -1,9 +1,8 @@
 <?php
 /**
- * Copyright © Magento, Inc. All rights reserved.
+ * Copyright ©  All rights reserved.
  * See COPYING.txt for license details.
  */
-
 use Magento\Framework\Component\ComponentRegistrar;
 
 ComponentRegistrar::register(ComponentRegistrar::MODULE, 'Agsoftware_Header', __DIR__);
